40ff94d39972121c945dfa9aaf345ca8ff560e59
marie-george <maria85night@gmail.com>
2023-09-08 13:36:41 +0300

    added docker-compose

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-yhzeu71&2$m^1r6jkwk=1hy+-k!8wqb!q=k48%#u2uhwll74v%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -90,10 +90,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'good_habits',
-        'USER': 'postgres',
-        'PASSWORD': os.getenv('postgres_password')
+        'ENGINE': os.getenv('ENGINE'),
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT')
     }
 }
 
@@ -133,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
